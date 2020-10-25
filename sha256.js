4a535dfdb53fc6ad66903f69c5d910fecb4c4c0c
eduardocasi <50718767+eduardocasi@users.noreply.github.com>
2020-10-24 22:53:31 -0300

    Add files via upload

---
+++ sha256.js
@@ -0,0 +1,12 @@
+/* Simple sha256 code */
+var crypto = require('crypto');
+
+var hash = crypto.createHash('sha256');
+
+var code = 'Hello';
+
+code = hash.update(code);
+code = hash.digest(code);
+
+console.log(code);
+process.stdout.write(code.toString("hex"));
